836ef6ea2e491398094ed84f1a511789b6a76c9b
PAU45 <paulo.melendez@tecsup.edu.pe>
2025-05-12 01:40:26 -0500

    erro3

--- models/associations.js
+++ models/associations.js
@@ -1,14 +1,39 @@
-// filepath: c:\Users\paulito\Documents\examen02-coello\backend\models\associations.js
 const TipoMedico = require('./TipoMedico');
+const Especialidad = require('./Especialidad');
+const Laboratorio = require('./Laboratorio');
 const Medicamento = require('./Medicamento');
+const OrdenCompra = require('./OrdenCompra');
+const DetalleOrdenCompra = require('./DetalleOrdenCompra');
+const OrdenVenta = require('./OrdenVenta');
+const DetalleOrdenVta = require('./DetalleOrdenVta');
+const User = require('./User');
 
-// Define las relaciones
-TipoMedico.hasMany(Medicamento, {
-    foreignKey: 'tipoMedicoId',
-    as: 'medicamentos'
-});
+// Relaciones TipoMedico - Medicamento
+TipoMedico.hasMany(Medicamento, { foreignKey: 'CodTipoMed', as: 'medicamentos' });
+Medicamento.belongsTo(TipoMedico, { foreignKey: 'CodTipoMed', as: 'tipoMedico' });
 
-Medicamento.belongsTo(TipoMedico, {
-    foreignKey: 'tipoMedicoId',
-    as: 'tipoMedico'
-});
+// Relaciones Especialidad - Medicamento
+Especialidad.hasMany(Medicamento, { foreignKey: 'CodEspec', as: 'medicamentos' });
+Medicamento.belongsTo(Especialidad, { foreignKey: 'CodEspec', as: 'especialidad' });
+
+// Relaciones Laboratorio - OrdenCompra
+Laboratorio.hasMany(OrdenCompra, { foreignKey: 'CodLab', as: 'ordenesCompra' });
+OrdenCompra.belongsTo(Laboratorio, { foreignKey: 'CodLab', as: 'laboratorio' });
+
+// Relaciones OrdenCompra - DetalleOrdenCompra
+OrdenCompra.hasMany(DetalleOrdenCompra, { foreignKey: 'NroOrdenC', as: 'detalles' });
+DetalleOrdenCompra.belongsTo(OrdenCompra, { foreignKey: 'NroOrdenC', as: 'ordenCompra' });
+
+// Relaciones Medicamento - DetalleOrdenCompra
+Medicamento.hasMany(DetalleOrdenCompra, { foreignKey: 'CodMedicamento', as: 'detallesCompra' });
+DetalleOrdenCompra.belongsTo(Medicamento, { foreignKey: 'CodMedicamento', as: 'medicamento' });
+
+// Relaciones OrdenVenta - DetalleOrdenVta
+OrdenVenta.hasMany(DetalleOrdenVta, { foreignKey: 'NroOrdenVta', as: 'detalles' });
+DetalleOrdenVta.belongsTo(OrdenVenta, { foreignKey: 'NroOrdenVta', as: 'ordenVenta' });
+
+// Relaciones Medicamento - DetalleOrdenVta
+Medicamento.hasMany(DetalleOrdenVta, { foreignKey: 'CodMedicamento', as: 'detallesVenta' });
+DetalleOrdenVta.belongsTo(Medicamento, { foreignKey: 'CodMedicamento', as: 'medicamento' });
+
+// Puedes agregar más relaciones según tu lógica de negocio
