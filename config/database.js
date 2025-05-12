7c36e2295947de43154386a46e81aaf1d8d6bc4c
PAU45 <paulo.melendez@tecsup.edu.pe>
2025-05-12 00:33:32 -0500

    database

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
-// filepath: c:\Users\paulito\Documents\examen02-coello\backend\config\database.js
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306
-});
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT, // debe ser 'postgres'
+  }
+);
 
 module.exports = sequelize;
