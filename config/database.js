15a134a13655fe876a7b8e00130dea78fbb72d1c
PAU45 <paulo.melendez@tecsup.edu.pe>
2025-05-12 00:36:06 -0500

    .post

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT, // debe ser 'postgres'
+    dialect: process.env.DB_DIALECT || 'postgres', // valor por defecto
   }
 );
 
